9d6d8b94fee2b7b5c1ef467b19ba0e0d4aa48152
nadeeboy <nadeejasamarakoon1@gmail.com>
2024-10-26 22:13:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,5 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
 MODE: process.env.MODE || "public", //private or public
-AUTO_VOICE:"true" //true or false
+AUTO_VOICE:"false" //true or false
 };
