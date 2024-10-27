ba719db447668c30bac4262c2474545b56ce9a03
nadeeboy <nadeejasamarakoon1@gmail.com>
2024-10-27 19:12:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "djknRSKD#1intBs9AplBfnBaquG_pfjU3ojBBCqzzvL4rXlr6Ufc", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeeja*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
 MODE: process.env.MODE || "public", //private or public
 AUTO_VOICE:"false" //true or false
